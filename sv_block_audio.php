d8620df94775e3674f599379ec131ba6083d5501
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-20 20:03:48 +0200

    support for site editor templates

--- sv_block_audio.php
+++ sv_block_audio.php
@@ -11,6 +11,7 @@
 				->set_section_template_path()
 				->set_section_order(5000)
 				->set_section_icon('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 600 600"><path d="M470.38 1.51L150.41 96A32 32 0 0 0 128 126.51v261.41A139 139 0 0 0 96 384c-53 0-96 28.66-96 64s43 64 96 64 96-28.66 96-64V214.32l256-75v184.61a138.4 138.4 0 0 0-32-3.93c-53 0-96 28.66-96 64s43 64 96 64 96-28.65 96-64V32a32 32 0 0 0-41.62-30.49z"/></svg>')
+				->set_block_handle('wp-block-audio')
 				->get_root()
 				->add_section( $this );
 		}
